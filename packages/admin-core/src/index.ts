d050e0a8740f1dad3aaa81438fa739e8b1daa18b
Ethan Sweet <ethan@sweetmediaservices.com>
2026-05-02 17:07:37 -0700

    Add useBlogQueue to admin core

--- packages/admin-core/src/index.ts
+++ packages/admin-core/src/index.ts
@@ -12,3 +12,4 @@ export { AuthProvider, useAuth } from "./contexts/AuthContext";
 export { default as AdminGuard } from "./components/AdminGuard";
 export { default as AdminSidebar } from "./components/AdminSidebar";
 export { default as AdminChrome } from "./components/AdminChrome";
+export { useBlogQueue } from "./hooks/useBlogQueue";
